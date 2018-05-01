9a64a24fca6588848166bea2fc995ff64f8f6988
Manuel Calvino <mcalvinolaguardia@gmail.com>
2018-04-30 23:37:55 -0400

    added redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,18 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 //import thunk from 'redux-thunk'; - don't know why would I use it yet
+import { createLogger } from 'redux-logger'
 import rootReducer from './reducers'
 import { input }  from './middleware/input'
 
-console.log(input);
+
+const loggerMiddleware = createLogger()
 
 export default createStore(
     rootReducer,
     compose(
         applyMiddleware(
-            input
+            input,
+            loggerMiddleware
         )
     )
 )
