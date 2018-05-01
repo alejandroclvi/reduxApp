0f14cbf97cc87db1022b58306e25eea8a9d7c76d
Manuel Calvino <mcalvinolaguardia@gmail.com>
2018-05-01 11:32:21 -0400

    fixed getVisibleTodoList logic(was inversed)

--- src/components/container/VisibleTodoList.js
+++ src/components/container/VisibleTodoList.js
@@ -12,9 +12,9 @@ getVisibleTodos = (todos, visibilityFilter) => {
         case 'SHOW_ALL':
             return todos;
         case 'SHOW_ACTIVE':
-            return todos.filter(todo => todo.completed)
+            return todos.filter(todo => !todo.completed)
         case 'SHOW_COMPLETED':
-            return todos.filter( todo => !todo.completed)
+            return todos.filter( todo => todo.completed)
         default:
             return todos
     }
